full fit, todo norm data leak fix

model/ma.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 from constants import TS_MIN_SEQUENCE_LEN
 
 
-class MovingAverageType(Enum):
+class MAType(Enum):
     SimpleMA = 0,
     WeightedMA = 1,
-    ExponentialMA = 2,
+    ExponentMA = 2,
 
 
 def get_simple_moving_average(ts_data: np.ndarray | pd.Series, window: int=TS_MIN_SEQUENCE_LEN) -> np.ndarray:
@@ -74,15 +74,15 @@ def get_exponential_moving_average(ts_data: np.ndarray | pd.Series, window: int=
 
 
 def plot_ma_for_timeseries(ts_data: np.ndarray | pd.Series, window: int=TS_MIN_SEQUENCE_LEN, weights: list[int]=None,
-                           ma_type: MovingAverageType=MovingAverageType.SimpleMA):
+                           ma_type: MAType=MAType.SimpleMA):
     match ma_type:
-        case MovingAverageType.SimpleMA:
+        case MAType.SimpleMA:
             ma_label = "SMA"
             vals = get_simple_moving_average(ts_data, window)
-        case MovingAverageType.WeightedMA:
+        case MAType.WeightedMA:
             ma_label = "WMA"
             vals = get_weighted_moving_average(ts_data, window, weights)
-        case MovingAverageType.ExponentialMA:
+        case MAType.ExponentMA:
             ma_label = "EMA"
             vals = get_exponential_moving_average(ts_data, window)
         case _:
@@ -105,6 +105,6 @@ if __name__ == "__main__":
     from_iso = convert_datetime_api_format(datetime.fromisoformat("2024-01-01"))
     to_iso = convert_datetime_api_format(datetime.fromisoformat("2026-01-01"))
     ts_data = get_candles_seq_uni(from_iso, to_iso, YDEX_TICKER, to_cache=True).reshape(-1)
-    plot_ma_for_timeseries(ts_data, window=10, ma_type=MovingAverageType.ExponentialMA)
-    plot_ma_for_timeseries(ts_data, window=20, ma_type=MovingAverageType.ExponentialMA)
-    plot_ma_for_timeseries(ts_data, window=50, ma_type=MovingAverageType.ExponentialMA)
+    plot_ma_for_timeseries(ts_data, window=10, ma_type=MAType.ExponentMA)
+    plot_ma_for_timeseries(ts_data, window=20, ma_type=MAType.ExponentMA)
+    plot_ma_for_timeseries(ts_data, window=50, ma_type=MAType.ExponentMA)

model/mlp.py
@@ -8,7 +8,7 @@ from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingRandomSearchCV
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error, median_absolute_error, mean_absolute_percentage_error
 
-from preproc.xy import split_seq_xy_pipe
+from preproc.xy import split_sequence, split_seq_xy_pipe, NormType, normalize_seq_uni
 from constants import RANDOM_STATE, MAX_ITER, CV_FOLDS, TS_MIN_SEQUENCE_LEN, TS_MAX_SEQUENCE_LEN, TEST_SIZE
 
 
@@ -30,13 +30,14 @@ def train_mlp_uni_reg(mlp_reg: MLPRegressor, X_train: np.ndarray, y_train: np.nd
         search = HalvingRandomSearchCV(mlp_reg, param_distributions=param_distr, n_candidates="exhaust", factor=1.5, refit=True,
                                        scoring="neg_root_mean_squared_error", cv=cv, random_state=random_state, n_jobs=2, verbose=verbose,
                                        ).fit(X_train, y_train)
-        return search
+        return search.best_estimator_
 
 
 def outer_seq_len_search(init_mlp_reg: MLPRegressor, val_metric: str, ts_seq: np.ndarray, test_size: float=TEST_SIZE,
                          min_len: int=TS_MIN_SEQUENCE_LEN, max_len: int=TS_MAX_SEQUENCE_LEN,
                          param_distr: dict=None, cv: int=CV_FOLDS, verbose: int=0, random_state: int=RANDOM_STATE
                          ) -> Tuple[MLPRegressor, int]:
+    """Training multiple MLP to find best sequence length for prediction"""
 
     best_val_error = float("inf")
     best_mlp_reg = None
@@ -53,6 +54,27 @@ def outer_seq_len_search(init_mlp_reg: MLPRegressor, val_metric: str, ts_seq: np
             print(f"New best '{val_metric}': {best_val_error:.6f} with sequence_len {best_seq_len}")
 
     return best_mlp_reg, best_seq_len
+
+
+def fit_mlp_uni_reg(mlp_reg: MLPRegressor, ts_seq: np.ndarray, seq_len: int, norm_type: NormType=NormType.NoNorm,
+                    random_state: int=RANDOM_STATE) -> MLPRegressor:
+    """Fit the best trained model on all data for future predictions"""
+    ts_seq = normalize_seq_uni(ts_seq, norm_type)
+    X, y = split_sequence(ts_seq, seq_len)
+    mlp_reg.fit(X, y)
+    return mlp_reg
+
+
+# TODO: we have to pass a Scaler object for particular fitted model, also denormalize final prediction
+def predict_uni_next_price(mlp_reg: MLPRegressor, ts_seq: np.ndarray, seq_len: int, norm_type: NormType=NormType.NoNorm)\
+        -> float:
+    """Predict next price with fit model and its sequence len"""
+    if len(ts_seq) < seq_len:
+        raise ValueError(f"Need a sequence with len >={seq_len} for prediction")
+
+    input_seq = ts_seq[-seq_len:].reshape(1, -1)
+    pred = mlp_reg.predict(input_seq)[0]
+    return pred
 
 
 def log_metrics(metrics: Dict[str, Dict[str, float]]):

preproc/xy.py
@@ -1,10 +1,6 @@
 from typing import Tuple
 import os
 import sys
-
-from matplotlib.patheffects import Normal
-from sympy import sequence
-
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from enum import Enum
 import numpy as np
@@ -15,8 +11,8 @@ from constants import TS_MAX_SEQUENCE_LEN, CACHE_DIR_FPATH, CANDLES_UNI_FEATURE,
 from data.candles import get_candles_data, get_candles_df
 
 
-class NormalizationType(Enum):
-    NoNormalization = 0,
+class NormType(Enum):
+    NoNorm = 0,
     Standardize = 1,
     MinMax = 2,
 
@@ -47,14 +43,15 @@ def get_candles_seq_uni(from_utc: str, to_utc: str, instrument_id: str, interval
     return sequence
 
 
-def normalize_seq_uni(ts_data: np.ndarray, norm_type: NormalizationType=NormalizationType.Standardize) -> np.ndarray:
+# TODO: I guess fitting scaler on full sequence and not just X_train isn't a good idea brotherman
+def normalize_seq_uni(ts_data: np.ndarray, norm_type: NormType=NormType.Standardize) -> np.ndarray:
     match norm_type:
-        case NormalizationType.NoNormalization:
+        case NormType.NoNorm:
             sequence = ts_data.reshape(-1)
-        case NormalizationType.Standardize:
+        case NormType.Standardize:
             sc = StandardScaler()
             sequence = sc.fit_transform(ts_data).reshape(-1)
-        case NormalizationType.MinMax:
+        case NormType.MinMax:
             mm = MinMaxScaler()
             sequence = mm.fit_transform(ts_data).reshape(-1)
         case _: